e9d4a737f1485e7ac06811b7fead51a4a71cb459
AJ Snarr <ajsnarr98@gmail.com>
2020-07-22 17:58:57 -0400

    added fat jar building to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,50 @@
 plugins {
-    application
-    kotlin("jvm") version "1.3.70"
-    c
+  application
+  kotlin("jvm") version "1.3.70"
+  java
+  c
 }
 
 application {
-    mainClassName = "com.ajsnarr.main.MainKt"
+  mainClassName = "com.ajsnarr.main.MainKt"
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
+  compile(kotlin("stdlib"))
 }
 
 repositories {
-    jcenter()
-    maven { url = uri("https://jitpack.io") }
+  jcenter()
+  maven { url = uri("https://jitpack.io") }
+}
+
+sourceSets {
+  main {
+    java {
+      srcDirs(
+              "src/main/kotlin/"
+      )
+    }
+    jni {
+      srcDirs = []
+    }
+
+    jniLibs {
+      srcDir="src/main/libs"
+    }
+  }
+}
+
+tasks.jar {
+  manifest {
+    attributes("Main-Class" to "com.ajsnarr.main.MainKt")
+  }
+
+  // To add all of the dependencies
+  from(sourceSets.main.get().output)
+
+  dependsOn(configurations.runtimeClasspath)
+  from({
+    configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+  })
 }
