e29e2961c094f4c957fc66e3fb8476e67fea5c89
AJ Snarr <ajsnarr98@gmail.com>
2020-07-23 15:43:19 -0400

    added quick test to main

--- src/main/kotlin/com/ajsnarr/main/Main.kt
+++ src/main/kotlin/com/ajsnarr/main/Main.kt
@@ -1,14 +1,23 @@
 package com.ajsnarr.main
 
 import com.ajsnarr.hardware.GPIO;
+import com.ajsnarr.hardware.NEMA17Stepper
 
 fun main(args: Array<String>) {
-  println("Hello, world!")
+    println("Hello, world!")
 
-  val isInit = GPIO.initialize();
-  if (isInit) {
-    GPIO.terminate();
-  } else {
-    println("Failed to initialize GPIO")
-  }
+    val isInit = GPIO.initialize();
+    if (!isInit) {
+        println("Failed to initialize GPIO")
+        return
+    }
+
+    val motor = NEMA17Stepper(dirPin = 20, stepPin = 21)
+    motor.rotate(2.0, .75)
+
+    println("sleeping...")
+    Thread.sleep(5000)
+    println("...slept...")
+
+    GPIO.onShutdown();
 }
