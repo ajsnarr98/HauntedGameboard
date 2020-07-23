70960af4d9f456feedd92a583057768924795030
AJ Snarr <ajsnarr98@gmail.com>
2020-07-23 19:30:22 -0400

    added constants file

---
+++ src/main/kotlin/com/ajsnarr/hardware/Constants.kt
@@ -0,0 +1,11 @@
+package com.ajsnarr.hardware
+
+object Constants {
+    object StepMot {
+        const val STEP_PIN_1 = 21
+        const val DIR_PIN_1 = 20
+
+        const val STEP_PIN_2 = 26
+        const val DIR_PIN_2 = 19
+    }
+}
