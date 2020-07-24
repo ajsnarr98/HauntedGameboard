5af95d03d06135d92de0df2736da06e1a078a220
AJ Snarr <ajsnarr98@gmail.com>
2020-07-24 08:39:33 -0400

    added stubbed out concrete rails class

---
+++ src/main/kotlin/com/ajsnarr/hauntedgameboard/hardware/rails/NEMA17Rails.kt
@@ -0,0 +1,45 @@
+package com.ajsnarr.hauntedgameboard.hardware.rails
+
+import com.ajsnarr.hauntedgameboard.hardware.steppermotor.NEMA17Stepper
+import com.ajsnarr.hauntedgameboard.hardware.steppermotor.StepperMotor
+
+/**
+ * 2D Rail system using two NEMA 17 Stepper motors.
+ *
+ * @param dirPinX gpio pin for x motor direction
+ * @param stepPinX gpio pin for x motor step
+ * @param dirPinY gpio pin for y motor direction
+ * @param stepPinY gpio pin for y motor step
+ */
+class NEMA17Rails(dirPinX: Int, stepPinX: Int, dirPinY: Int, stepPinY: Int) : AbstractLinearRail2D() {
+
+    override val motX: StepperMotor = NEMA17Stepper(dirPin = dirPinX, stepPin = stepPinX)
+    override val motY: StepperMotor = NEMA17Stepper(dirPin = dirPinY, stepPin = stepPinY)
+
+    override val rotationsPerX: Double
+        get() = TODO("Not yet implemented")
+    override val rotationsPerY: Double
+        get() = TODO("Not yet implemented")
+    override var MIN_X: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var MIN_Y: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var MAX_X: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var MAX_Y: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var x: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var y: Double
+        get() = TODO("Not yet implemented")
+        set(value) {}
+
+    override fun resetPos() {
+        TODO("Not yet implemented")
+    }
+}
