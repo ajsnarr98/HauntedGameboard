beb6ff089eb57fdad9cf0b3c925c7baeef40988f
AJ Snarr <ajsnarr98@gmail.com>
2020-07-28 15:56:47 -0400

    added more stuff for picamera

--- src/main/kotlin/com/ajsnarr/hauntedgameboard/hardware/camera/PiCamera.kt
+++ src/main/kotlin/com/ajsnarr/hauntedgameboard/hardware/camera/PiCamera.kt
@@ -1,16 +1,31 @@
 package com.ajsnarr.hauntedgameboard.hardware.camera
 
+import uk.co.caprica.picam.CameraConfiguration
 import uk.co.caprica.picam.PicamNativeLibrary.installTempLibrary
 
-class PiCamera : Camera {
+open class PiCamera(config: Configuration) : Camera {
+
+    /**
+     * Wraps a caprica.picam CameraConfiguration.
+     *
+     * There are more functions available than wrapped here.
+     */
+    class Configuration() {
+        private val config = CameraConfiguration.cameraConfiguration()
+
+        /**
+         * This method should only be called by the PiCamera class.
+         */
+        internal fun build() = uk.co.caprica.picam.Camera(config)
+    }
 
     companion object {
         // static initializer
         init {
-            // load native library for picam
             // Extract the bundled picam native library to a temporary file and load it
             installTempLibrary();
         }
     }
 
+    val camera = config.build()
 }
