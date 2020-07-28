a87e3e1ebbf6a2e8488ba742570355e6e08c8aa6
AJ Snarr <ajsnarr98@gmail.com>
2020-07-28 18:00:48 -0400

    add wrappers to pi camera

--- src/main/kotlin/com/ajsnarr/hauntedgameboard/hardware/camera/PiCamera.kt
+++ src/main/kotlin/com/ajsnarr/hauntedgameboard/hardware/camera/PiCamera.kt
@@ -1,9 +1,26 @@
 package com.ajsnarr.hauntedgameboard.hardware.camera
 
 import uk.co.caprica.picam.CameraConfiguration
+import uk.co.caprica.picam.FilePictureCaptureHandler
 import uk.co.caprica.picam.PicamNativeLibrary.installTempLibrary
+import uk.co.caprica.picam.enums.Encoding
+import java.io.File
 
 open class PiCamera(config: Configuration) : Camera {
+
+    /**
+     * Picture encoding.
+     */
+    enum class Encoding(internal val wrappedVal: uk.co.caprica.picam.enums.Encoding) {
+        BGR24(uk.co.caprica.picam.enums.Encoding.BGR24),
+        BMP(uk.co.caprica.picam.enums.Encoding.BMP),
+        GIF(uk.co.caprica.picam.enums.Encoding.GIF),
+        I420(uk.co.caprica.picam.enums.Encoding.I420),
+        JPEG(uk.co.caprica.picam.enums.Encoding.JPEG),
+        OPAQUE(uk.co.caprica.picam.enums.Encoding.OPAQUE),
+        PNG(uk.co.caprica.picam.enums.Encoding.PNG),
+        RGB24(uk.co.caprica.picam.enums.Encoding.RGB24)
+    }
 
     /**
      * Wraps a caprica.picam CameraConfiguration.
@@ -11,7 +28,14 @@ open class PiCamera(config: Configuration) : Camera {
      * There are more functions available than wrapped here.
      */
     class Configuration() {
+
         private val config = CameraConfiguration.cameraConfiguration()
+
+        fun encoding(encoding: Encoding): Configuration = this.also {
+            config.encoding(encoding.wrappedVal)
+        }
+
+        fun encoding(): Encoding? = Encoding.values().find { it.wrappedVal == config.encoding() }
 
         /**
          * This method should only be called by the PiCamera class.
@@ -27,5 +51,13 @@ open class PiCamera(config: Configuration) : Camera {
         }
     }
 
-    val camera = config.build()
+    private val camera = config.build()
+
+    fun capture(saveTo: File, captureMillis: Int? = null) {
+        if (captureMillis == null) {
+            camera.takePicture(FilePictureCaptureHandler(saveTo))
+        } else {
+            camera.takePicture(FilePictureCaptureHandler(saveTo), captureMillis)
+        }
+    }
 }
