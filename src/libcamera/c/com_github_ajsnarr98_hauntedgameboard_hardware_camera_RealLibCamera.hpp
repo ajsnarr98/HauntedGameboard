fb40a820f5db51f50d3fd575e4bfffa48da3f83d
AJ Snarr <ajsnarr98@gmail.com>
2022-08-26 16:55:07 -0400

    stop using template type for counting semaphore

--- src/libcamera/c/com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.hpp
+++ src/libcamera/c/com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.hpp
@@ -146,7 +146,7 @@ private:
 	std::set<Request *> completed_requests_;
 	bool camera_started_ = false;
 	std::mutex completed_requests_mutex_;
-	std::counting_semaphore<127> camera_requests_active(0);
+	std::counting_semaphore camera_requests_active(0);
 	ControlList controls_;
 	Stream still_stream_;
 
