a9de611dfe190415a14b5fa28ffd165688033152
AJ Snarr <ajsnarr98@gmail.com>
2022-09-05 13:56:23 -0400

    Add libcamera to create_so.sh

--- create_so.sh
+++ create_so.sh
@@ -2,6 +2,8 @@
 
 # this script should be run from the project home directory
 
+
+# GPIO
 # make shared lib
 cd src/gpio/c
 make
@@ -9,3 +11,12 @@ make
 # move to location
 cd ../../..
 mv src/gpio/c/gpio.so src/lib/gpio.so
+
+# LibCamera
+# make shared lib
+cd src/libcamera/c
+make
+
+# move to location
+cd ../../..
+mv src/libcamera/c/libcamera.so src/lib/libcamera.so
--- src/libcamera/c/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-make clean
-rm Makefile
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm compile_commands.json
-
