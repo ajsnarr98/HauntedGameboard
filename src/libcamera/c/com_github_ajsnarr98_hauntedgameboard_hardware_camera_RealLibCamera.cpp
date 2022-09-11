39e5b866830a7a06f1f067446c1536804d304521
AJ Snarr <ajsnarr98@gmail.com>
2022-09-11 15:57:39 -0400

    use unmodified jni h file

--- src/libcamera/c/com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.cpp
+++ src/libcamera/c/com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.cpp
@@ -1,7 +1,31 @@
 #ifndef _Included_com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera_c
 #define _Included_com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera_c
 
-#include "com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.hpp"
+#include "com_github_ajsnarr98_hauntedgameboard_hardware_camera_RealLibCamera.h"
+
+#include <sys/mman.h>
+
+#include <condition_variable>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <set>
+#include <string>
+#include <thread>
+#include <variant>
+
+#include <libcamera/base/span.h>
+#include <libcamera/camera.h>
+#include <libcamera/camera_manager.h>
+#include <libcamera/control_ids.h>
+#include <libcamera/controls.h>
+#include <libcamera/formats.h>
+#include <libcamera/framebuffer_allocator.h>
+#include <libcamera/property_ids.h>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -12,6 +36,89 @@
 bool showDebugLog = true;
 
 const std::string& LOG_PREFIX = "Libcamera: ";
+
+namespace controls = libcamera::controls;
+namespace properties = libcamera::properties;
+
+class LibcameraUsage
+{
+public:
+	using Stream = libcamera::Stream;
+	using FrameBuffer = libcamera::FrameBuffer;
+	using ControlList = libcamera::ControlList;
+	using Request = libcamera::Request;
+	using CameraManager = libcamera::CameraManager;
+	using Camera = libcamera::Camera;
+	using CameraConfiguration = libcamera::CameraConfiguration;
+	using ColorSpace = libcamera::ColorSpace;
+	using FrameBufferAllocator = libcamera::FrameBufferAllocator;
+	using StreamRole = libcamera::StreamRole;
+	using StreamRoles = libcamera::StreamRoles;
+	using PixelFormat = libcamera::PixelFormat;
+	using StreamConfiguration = libcamera::StreamConfiguration;
+	using BufferMap = Request::BufferMap;
+	using Size = libcamera::Size;
+	using Rectangle = libcamera::Rectangle;
+
+	// Error codes
+	static constexpr int SUCCESS = 0;
+	static constexpr int ERR_CAMERA_MANAGER_FAILED_TO_START = 1;
+	static constexpr int ERR_NO_CAMERAS_AVAILABLE = 2;
+	static constexpr int ERR_FAILED_TO_FIND_CAMERA = 3;
+	static constexpr int ERR_FAILED_TO_ACQUIRE_CAMERA = 4;
+	static constexpr int ERR_FAILED_TO_GENERATE_CAMERA_CONFIG = 5;
+	static constexpr int ERR_FAILED_TO_VALIDATE_STREAM_CONFIGURATIONS = 6;
+	static constexpr int ERR_FAILED_TO_CONFIGURE_STREAMS = 7;
+	static constexpr int ERR_FAILED_TO_ALLOCATE_CAPTURE_BUFFERS = 8;
+	static constexpr int ERR_REQUEST_CREATION_FAILED = 9;
+	static constexpr int ERR_REQUEST_CONCURRENT_STREAMS_WITHOUT_MATCHING_NUMBER_OF_BUFFERS = 10;
+	static constexpr int ERR_REQUEST_COULD_NOT_ADD_BUFFER_TO_REQUEST = 11;
+	static constexpr int ERR_FAILED_TO_START_CAMERA = 12;
+	static constexpr int ERR_CAMERA_FAILED_TO_QUEUE_REQUEST = 13;
+	static constexpr int ERR_CAPTURE_STOPPED = 14;
+	static constexpr int ERR_WIDTH_OR_HEIGHT_IS_NOT_EVEN = 14;
+	static constexpr int ERR_NON_SINGLE_PLANE_YUV_FOUND = 14;
+
+	LibcameraUsage();
+
+	std::string const &CameraId() const;
+
+	int AcquireCamera();
+	int ReleaseCamera();
+	int Configure();
+
+	void Teardown();
+	int StartCapture();
+	int CleanupAndStopCapture();
+	int Wait();
+
+	Stream *StillStream() const;
+
+//protected:
+//	std::unique_ptr<Options> options_;
+
+private:
+	std::unique_ptr<CameraManager> camera_manager_;
+	std::shared_ptr<Camera> camera_;
+	std::unique_ptr<CameraConfiguration> configuration_;
+	bool camera_acquired_ = false;
+	FrameBufferAllocator *allocator_ = nullptr;
+	std::map<FrameBuffer *, std::vector<libcamera::Span<uint8_t>>> mapped_buffers_;
+	std::map<Stream *, std::queue<FrameBuffer *>> frame_buffers_;
+	std::vector<std::unique_ptr<Request>> requests_;
+	std::set<Request *> completed_requests_;
+	bool camera_started_ = false;
+	std::mutex camera_stop_mutex_;
+	std::mutex requests_mutex_;
+	unsigned int num_requests_completed_ = 0;
+	std::condition_variable requests_cond_;
+	ControlList controls_;
+	Stream *still_stream_;
+
+	int setupCapture();
+	int makeRequests();
+	void requestComplete(Request *request);
+};
 
 static bool check_camera_stack();
 
