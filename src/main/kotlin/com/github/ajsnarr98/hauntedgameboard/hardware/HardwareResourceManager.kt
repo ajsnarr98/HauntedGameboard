cff09112d04fdcd29f48db0fa94727f2ef948cec
AJ Snarr <aj.snarr@willowtreeapps.com>
2022-10-01 12:52:34 -0400

    Only initialize gpio on main thread

--- src/main/kotlin/com/github/ajsnarr98/hauntedgameboard/hardware/HardwareResourceManager.kt
+++ src/main/kotlin/com/github/ajsnarr98/hauntedgameboard/hardware/HardwareResourceManager.kt
@@ -6,7 +6,10 @@ import com.github.ajsnarr98.hauntedgameboard.hardware.camera.RealLibCamera
 import com.github.ajsnarr98.hauntedgameboard.hardware.gpiointerface.FakeGPIO
 import com.github.ajsnarr98.hauntedgameboard.hardware.gpiointerface.GPIOInterface
 import com.github.ajsnarr98.hauntedgameboard.hardware.gpiointerface.RealGPIO
+import com.github.ajsnarr98.hauntedgameboard.util.DefaultDispatcherProvider
+import com.github.ajsnarr98.hauntedgameboard.util.DispatcherProvider
 import com.github.ajsnarr98.hauntedgameboard.util.Initializable
+import kotlinx.coroutines.withContext
 import java.io.Closeable
 
 interface HardwareResourceManager : Initializable, Closeable {
@@ -22,6 +25,7 @@ interface HardwareResourceManager : Initializable, Closeable {
 }
 
 class DefaultHardwareResourceManager(
+    val dispatcherProvider: DispatcherProvider,
     cameraConstructor: () -> Camera = {
         try {
             RealLibCamera()
@@ -56,10 +60,19 @@ class DefaultHardwareResourceManager(
     override val camera: Camera = cameraConstructor()
     override val gpio: GPIOInterface = gpioConstructor()
 
+    /* Native gpio must be initialized on main thread. */
+    private val mustInitializeOnMainThread = hashSetOf<Initializable>(gpio)
+
     override suspend fun initialize(): Boolean {
         val initialized = mutableListOf<Initializable>()
+        var success: Boolean
         for (res in listOf<Initializable>(camera, gpio)) {
-            if (!res.initialize()) {
+            success = if (res in mustInitializeOnMainThread) {
+                withContext(dispatcherProvider.main()) { res.initialize() }
+            } else {
+                res.initialize()
+            }
+            if (!success) {
                 initialized.forEach { if (it is Closeable) it.close() }
                 throw HardwareResourceManager.HardwareInitializationException(res.javaClass.name)
             }
