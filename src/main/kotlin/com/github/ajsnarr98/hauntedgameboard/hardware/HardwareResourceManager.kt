6be3ba48de2ba0f3ac00c32ea4c9faca8deb3982
AJ Snarr <ajsnarr98@gmail.com>
2022-10-02 13:32:00 -0400

    Update HardwareResourceManager.kt

--- src/main/kotlin/com/github/ajsnarr98/hauntedgameboard/hardware/HardwareResourceManager.kt
+++ src/main/kotlin/com/github/ajsnarr98/hauntedgameboard/hardware/HardwareResourceManager.kt
@@ -57,8 +57,9 @@ class DefaultHardwareResourceManager(
         }
     },
 ) : HardwareResourceManager {
-    //    override val camera: Camera = cameraConstructor()
-    override val camera: Camera = FakeCamera()
+    override val camera: Camera = cameraConstructor()
+
+    //    override val camera: Camera = FakeCamera()
     override val gpio: GPIOInterface = gpioConstructor()
 
     /* Native gpio must be initialized on main thread. */
